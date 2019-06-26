67ea56ab1c1c9805616287cb0c5026884bdea6bb
Aleksandra Mitula-EXT <Aleksandra.Mitula@jeppesen.com>
2019-06-26 20:14:37 +0200

    Initial commit

---
+++ Dzien2/string_exercises.py
@@ -0,0 +1,10 @@
+imie = "Ola"
+nazwisko = "Mitula"
+print('witaj',imie,nazwisko)
+
+string1='witaj'+' '+imie+' '+nazwisko
+print(string1)
+print(type(string1))
+
+print('Witaj'+' '+imie+' '+nazwisko)
+print(f'Witaj {imie} {nazwisko}')
