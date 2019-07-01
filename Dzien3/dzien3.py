0b342d44e1db2273d6f1769e87b1c46849b5de0d
aleksandramitula <olamitolamit@gmail.com>
2019-07-01 18:15:14 +0200

    comment added

--- Dzien3/dzien3.py
+++ Dzien3/dzien3.py
@@ -1,8 +1,14 @@
-temperature = 28
+temperature = 25
+#DUZYMI LITERAMI piszemy stala, nie zmienna. To sa te wartosci, ktorych nie chcemy zmieniac w srodku w kodzie, ale mozna zmienic na poczatku:
+HOT_TEMPERATURE=30
+WARM_TEMPERATURE=25
+NOT_COLD_NOT_WARM_TEMPERATURE=20
 
-if temperature >= 30:
+if temperature >=HOT_TEMPERATURE:
     print("it's hot")
-elif temperature >=25:
+elif temperature >=WARM_TEMPERATURE:
     print("it's warm")
+elif temperature >=NOT_COLD_NOT_WARM_TEMPERATURE:
+    print("it's not warm or cold")
 else:
     print("it's cold")
