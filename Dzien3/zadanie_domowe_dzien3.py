5233dd65e53611170287b6a9158cd149aef69f57
aleksandramitula <olamitolamit@gmail.com>
2019-07-02 16:13:41 +0200

    ex. 3 added

--- Dzien3/zadanie_domowe_dzien3.py
+++ Dzien3/zadanie_domowe_dzien3.py
@@ -1,12 +1,23 @@
-#1.	Napisz program analogiczny do powyższego (nie przejmuj się jeżeli nie rozumiesz pierwszej linii po prostu ją przepisz), gdzie podasz 3 liczby oraz wypiszesz sumę trzech podanych liczb
+# #1.	Napisz program analogiczny do powyższego (nie przejmuj się jeżeli nie rozumiesz pierwszej linii po prostu ją przepisz), gdzie podasz 3 liczby oraz wypiszesz sumę trzech podanych liczb
+#
+# from sys import argv
+#
+# program_name, first_arg, second_arg, third_arg = argv
+#
+# wynik=int(first_arg)+int(second_arg)+int(third_arg)
+# print(f'Pierwszy argument: {first_arg}, Drugi argument: {second_arg}, Trzeci argument: {third_arg}, Suma argumentow: {wynik}')
+#
+# # #alternatywa - drugi print pod spodem:
+# # wynik=int(first_arg)+int(second_arg)+int(third_arg)
+# # print(f'Suma argumentow: {wynik}')
+
+
+
+#2.	Napisz program w którym podasz jako pierwszy argument zdanie, a drugi argument liczbę oznaczającą ile razy to zdanie powtórzyć. Wypisz tyle razy to zdanie – użyj pętli for in oraz klasy range
 
 from sys import argv
 
-program_name, first_arg, second_arg, third_arg = argv
+program_name, zdanie, powtorzenie = argv
 
-wynik=int(first_arg)+int(second_arg)+int(third_arg)
-print(f'Pierwszy argument: {first_arg}, Drugi argument: {second_arg}, Trzeci argument: {third_arg}, Suma argumentow: {wynik}')
-
-# #alternatywa - drugi print pod spodem:
-# wynik=int(first_arg)+int(second_arg)+int(third_arg)
-# print(f'Suma argumentow: {wynik}')
+for index in range(int(powtorzenie)):
+    print(zdanie)
