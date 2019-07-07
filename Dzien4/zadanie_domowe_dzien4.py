3901c32ad76056eafce8c88c20bec5d822fae086
aleksandramitula <olamitolamit@gmail.com>
2019-07-07 22:38:24 +0200

    update_homework_day4

--- Dzien4/zadanie_domowe_dzien4.py
+++ Dzien4/zadanie_domowe_dzien4.py
@@ -84,41 +84,60 @@
 # # czesci, testuj i powiekszaj
 #
 # # 1 stwórz kilka zmiennych różnych typów - int, float, boolean, string
-#
-int=35
-flo=20.5
-boo=True
-str='Witaj'
+
+# int=35
+# flo=20.5
+# boo=True
+# str='Witaj'
 
 # # 2 za pomocą funkcji print() wypisz wartości powyższych zmiennych, podając ich typ (użyj funkcji type)
 # # pamiętaj o formatowaniu i znakach specjalnych, najlepiej aby pokazywać wartości wraz z typami zmiennych
 # # w nowej linii
 
 
-print(f"Zmienna {int} - typ {type(int)}\r\nZmienna {flo} - typ {type(flo)}\r\nZmienna {boo} - typ {type(boo)}\r\nZmienna {str} - typ {type(str)}")
+# print(f"Zmienna {int} - typ {type(int)}\r\nZmienna {flo} - typ {type(flo)}\r\nZmienna {boo} - typ {type(boo)}\r\nZmienna {str} - typ {type(str)}")
 
 
-# # pamiętaj o możliwości specjalnego formatowania tzw. interpolacji:
-# # https://realpython.com/python-string-formatting/#3-string-interpolation-f-strings-python-36
-#
-# # formatowanie (slajd z dnia 3 - od strony 3)
-#
-# # 3. Wypisz zmienną typu float - dobrym przykladem bedzie liczba 1/17
-# # z dokładnością do 4 miejsc po przecinku
+# # 3. Wypisz zmienną typu float - dobrym przykladem bedzie liczba 1/17. z dokładnością do 4 miejsc po przecinku
 # # https://stackoverflow.com/questions/8885663/how-to-format-a-floating-number-to-fixed-width-in-python
-#
+
+# float_number=1/17
+# print(f'{float_number:5.4f}')
+
+
 # # 4. Indeksowanie (slajd dzien 3 od strony 4)
 # # stwórz nową zmienną typu string oraz zmienną pomocniczą, do której przy pomocy indeksowania,
 # # przypiszesz odwrócony string pierwszej zmiennej (możesz przejrzeć slajd 4 z prezentacji dnia 3 i sprawdzic np w konsoli
 # # które indeksowanie odwraca)
-#
-# my_string = "dsadasdad"
-# my_string_backwords = ???
-#
+
+# string="dziendobry"
+# odwrocenie=string[::-1]
+# print(odwrocenie)
+
 # # 5. rozszerz zadanie 4 - sprawdź czy string jest palindromem
+
+# string="dziendobry"
+# odwrocenie=string[::-1]
+# print(odwrocenie)
+#
+# if string==odwrocenie:
+#     print(f"Slowo {string} jest palindromem")
+# else:
+#     print(f"Slowo '{string}' nie jest palindromem")
+
 # # 6. przy pomocy funkcji input() rozszerz zadanie 5 tak aby uzytkownik wpisał słowo i program powiedział,
 # # czy wisane slowo jest palindromem
+
+# string=input("Wpisz slowo: ")
+# odwrocenie=string[::-1]
+# #print(odwrocenie)
 #
+# if string==odwrocenie:
+#     print(f"Slowo '{string}' jest palindromem.")
+# else:
+#     print(f"Slowo '{string}' nie jest palindromem.")
+
+
 # # pętle oraz instruckje warunkowe
 # # 7. wykorzystaj funkcje input() i zagraj z uzytkownikiem programu w grę RPG
 # # Poinformuj użytkownika że stoi na rozdrozu - moze isc prosto, w lewo, lub w prawo
@@ -126,21 +145,39 @@ print(f"Zmienna {int} - typ {type(int)}\r\nZmienna {flo} - typ {type(flo)}\r\nZm
 # # w zależności od wyboru ścieżki, poinformuj użytkownika kogo spotkał (dla Twojej wyobraźni sky is the limit)
 # # jeżeli użytkownik nie wpisał poprawnie kierunku - napisz że nie umie się grać...
 # # (do rozwiazania przyda się if elif else)
-#
+
+
+
+
+
 # # 8. rozszerz zadanie 7 w taki sposób, aby zachęcić użytkownika jeszcze raz do wyboru ścieżki, jeżeli
 # # nie wpisał komendy poprawnie
 # # (przyda się pętla while - mozesz zatrzymać skrypt ręcznie jeżeli w czasie wykonywania się zapętlił nie tak jak powinien)
-#
+
+
+
+
 # # 9. Klasa range - przy użyciu pętli for in oraz użyciu range, wypisz tylko liczby podzielne przez 7 w zakresie 0-77
 # # wskazówka: przyda się warunek if oraz modulo %
 # # 4 % 2 == 0 -> true
-#
+
+# for liczba in range(78):
+#     if liczba%7==0:
+#         print(liczba)
+
+
 # # 10. listy - []
 # # wygeneruj 2 listy liczb o tych samych rozmiarach, ale różnych wartościach
-#
+
+# lista1=[1,3,15,6,89]
+# lista2=[17,4,6,12,7]
+
+
 # # 11. stwórz nową listę, używając dwóch list z zadania 10. lista ma zawierać liczby z listy pierwszej podniesione
 # # przemnozone przez liczbę o tym samym indeksie z listy drugiej
+
+# lista3=[]
+# for x,y in zip(lista1,lista2):
+#     z=lista3.append(x*y)
 #
-# # [1, 2, 3]
-# # [3, 8, 2]
-# # -> [3, 16, 6]
+# print(lista3)
